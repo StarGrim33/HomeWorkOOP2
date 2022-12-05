2152a00cca00887d1155f631ec249a2401ef32bc
StarGrim33 <prosky33@gmail.com>
2022-12-05 21:25:15 +0300

    Сделал задачу

---
+++ HomeWorkOOp2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Numerics;
+
+namespace HomeWorkOOp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Renderer renderer = new Renderer();
+
+            Console.WriteLine("Введите позицию по X");
+            int userInputPositionX = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите позицию по Y");
+            int userInputPositionY = Convert.ToInt32(Console.ReadLine());
+
+            Player player = new Player(userInputPositionX, userInputPositionY);
+
+            Console.Clear();
+            renderer.DrawPlayer(player.PlayerPositionX, player.PlayerPositionY);
+        }
+    }
+
+    class Player
+    {
+        public Player(int playerPositionX, int playerPositionY)
+        {
+            PlayerPositionX = playerPositionX;
+            PlayerPositionY = playerPositionY;
+        }
+
+        public int PlayerPositionX { get; private set; }
+        public int PlayerPositionY { get; private set; }
+    }
+
+    class Renderer
+    {
+        public void DrawPlayer(int x, int y, char playerSymbol = '$')
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(playerSymbol);
+        }
+    }
+}
