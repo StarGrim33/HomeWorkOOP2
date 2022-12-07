1c31e0b385ecc572b055e26ae6f797a1bebf8a26
StarGrim33 <prosky33@gmail.com>
2022-12-07 12:12:59 +0300

    Исправление замечаний

--- HomeWorkOOp2/Program.cs
+++ HomeWorkOOp2/Program.cs
@@ -6,42 +6,85 @@ namespace HomeWorkOOp2
     {
         static void Main(string[] args)
         {
-            int userInputPositionX = 0;
-            int userInputPositionY = 0;
+            string? userInputPositionX = null;
+            string? userInputPositionY = null;
+            string? userInputChar = null;
+            int integerUserInputX = 0;
+            int integerUserInputY = 0;
+            char charPlayerInput = '@';
             Renderer renderer = new Renderer();
 
-            DefinitionPlayerPositions(ref userInputPositionX, ref userInputPositionY);
+            DefinitionPlayerPositions(userInputPositionX, userInputPositionY, userInputChar, ref integerUserInputX, ref integerUserInputY, ref charPlayerInput);
 
-            Player player = new Player(userInputPositionX, userInputPositionY);
+            Player player = new Player(integerUserInputX, integerUserInputY, charPlayerInput);
 
             Console.Clear();
-            renderer.DrawPlayer(player.PlayerPositionX, player.PlayerPositionY);
+            renderer.DrawPlayer(player.PlayerLocationX, player.PlayerLocationY, player.PlayerSign);
         }
 
-        static void DefinitionPlayerPositions(ref int userInputPositionX, ref int userInputPositionY)
+        static void DefinitionPlayerPositions(string userInputPositionX, string userInputPositionY, string userInputChar, ref int integerUserInputX, ref int integerUserInputY, ref char charPlayerInput)
         {
             Console.WriteLine("Введите позицию по X");
-            userInputPositionX = Convert.ToInt32(Console.ReadLine());
+            userInputPositionX = Console.ReadLine();
+
+            if (int.TryParse(userInputPositionX, out integerUserInputX))
+            {
+                Console.WriteLine("Позиция Х: " + integerUserInputX);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод, попробуйте заново, введя число");
+                Console.ReadKey();
+                return;
+            }
+
             Console.WriteLine("Введите позицию по Y");
-            userInputPositionY = Convert.ToInt32(Console.ReadLine());
+            userInputPositionY = Console.ReadLine();
+
+            if (int.TryParse(userInputPositionY, out integerUserInputY))
+            {
+                Console.WriteLine("Позиция Х: " + integerUserInputY);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод, попробуйте заново, введя число");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Введите символ отображения игрока: ");
+            userInputChar = Console.ReadLine();
+
+            if(char.TryParse(userInputChar, out charPlayerInput))
+            {
+                Console.WriteLine("Введен символ:" + userInputChar);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод, попробуйте заново");
+                Console.ReadKey();
+                return;
+            }
         }
     }
 
     class Player
     {
-        public Player(int playerPositionX, int playerPositionY)
+        public Player(int playerPositionX, int playerPositionY, char playerSymbol)
         {
-            PlayerPositionX = playerPositionX;
-            PlayerPositionY = playerPositionY;
+            PlayerLocationX = playerPositionX;
+            PlayerLocationY = playerPositionY;
+            PlayerSign = playerSymbol;
         }
 
-        public int PlayerPositionX { get; private set; }
-        public int PlayerPositionY { get; private set; }
+        public int PlayerLocationX { get; private set; }
+        public int PlayerLocationY { get; private set; }
+        public char PlayerSign { get; private set; }
     }
 
     class Renderer
     {
-        public void DrawPlayer(int playerPositionX, int playerPositionY, char playerSymbol = '$')
+        public void DrawPlayer(int playerPositionX, int playerPositionY, char playerSymbol)
         {
             Console.SetCursorPosition(playerPositionX, playerPositionY);
             Console.Write(playerSymbol);
